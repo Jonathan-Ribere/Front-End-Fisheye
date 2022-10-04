0bc05cc8eb6f2f5955fb4f86d1d029789a89379e
Jonathan Ribiere <j_ribiere@icloud.com>
2022-10-04 02:23:24 +0200

    création de la div qui englobe la photo de profil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,14 +4,26 @@ function photographerFactory(data) {
   const picture = `assets/photographers/${portrait}`;
 
   function display() {
+    // Créer l'article
     const article = document.createElement("article");
+
+    // Création de la div avec la class "enTete"
     const head = document.createElement("div");
     head.classList.add("enTete");
+    // Head(Div avec la class "enTete" et l'enfant de l'article)
     article.appendChild(head);
+
+    // Création de la div qui englobe la photo de profile
+    const divImg = document.createElement("div");
+    divImg.classList.add("divImg");
+    head.appendChild(divImg);
+
     const img = document.createElement("img");
     img.setAttribute("src", picture);
+    img.classList.add("classImg");
+    divImg.appendChild(img);
+
     const h2 = document.createElement("h2");
-    head.appendChild(img);
     head.appendChild(h2);
     h2.innerHTML = name;
 
