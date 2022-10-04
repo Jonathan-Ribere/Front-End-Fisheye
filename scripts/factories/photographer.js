5e41bdb676179ba2cafb143e2d27faf36637970e
Jonathan Ribiere <j_ribiere@icloud.com>
2022-10-04 02:35:29 +0200

    création de la div corps

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,6 +7,8 @@ function photographerFactory(data) {
     // Créer l'article
     const article = document.createElement("article");
 
+    /* DEBUT DE LA DIV ENTETE */
+
     // Création de la div avec la class "enTete"
     const head = document.createElement("div");
     head.classList.add("enTete");
@@ -23,18 +25,28 @@ function photographerFactory(data) {
     img.classList.add("classImg");
     divImg.appendChild(img);
 
+    // Création du h2 sous la photo de profile
     const h2 = document.createElement("h2");
     head.appendChild(h2);
     h2.innerHTML = name;
+
+    /* FIN DE LA DIV ENTETE */
+
+    /* DEBUT DE LA DIV CORPS */
+
+    const corps = document.createElement("div");
+    corps.classList.add("corps");
+    article.appendChild(corps);
 
     // Créer une Div
     const ville = document.createElement("div");
     // Ajoute une class
     ville.classList.add("ville");
     // info et l'enfant de article
-    article.appendChild(ville);
+    corps.appendChild(ville);
     // on recupére le text de la bd avec city & inject avec inner
     ville.innerHTML = city;
+    /* FIN DE LA DIV ENTETE */
 
     return article;
   }
