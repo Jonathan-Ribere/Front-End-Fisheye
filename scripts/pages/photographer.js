43f50a82f5f126534ed6f75743ab5d6f24e9c31c
Jonathan Ribiere <j_ribiere@icloud.com>
2022-10-05 02:22:46 +0200

    debut de la recup avec photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,16 @@
-async function getPhotographersProfil() {}
+(async function () {
+  const profilId = await getPhotographersId();
+  const profil = getPhotographersProfil(profilId);
+  console.log(profil);
+});
+
+function getPhotographersId() {
+  //Permet de recup l'id dans l'url
+  return new URL(location.href).searchParams.get("id");
+}
+
+async function getPhotographersProfil() {
+  const response = await fetch("/data/photographers.json");
+  const data = await response.json();
+  return data;
+}
