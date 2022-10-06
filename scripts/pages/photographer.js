eeb3292cc922e39b26f48a6af6781addef1bf4bd
Jonathan Ribiere <j_ribiere@icloud.com>
2022-10-07 01:22:29 +0200

    j'essaye de recup les donnée ds l'objet

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,16 +1,26 @@
-(async function () {
-  const profilId = await getPhotographersId();
-  const profil = getPhotographersProfil(profilId);
-  console.log(profil);
-});
+(async function noName() {
+  const articleId = getArticleId();
+  console.log(articleId);
+  const article = await getArticle(articleId);
+  display(article);
+})();
 
-function getPhotographersId() {
+function getArticleId() {
   //Permet de recup l'id dans l'url
   return new URL(location.href).searchParams.get("id");
 }
 
-async function getPhotographersProfil() {
+function getArticle(articleId) {
+  const response = fetch(`/data/photographers.json/${articleId}`);
+  const data = response.json();
+  return data;
+}
+
+function display(article) {}
+
+/*async function getProfil() {
   const response = await fetch("/data/photographers.json");
   const data = await response.json();
   return data;
 }
+*/
