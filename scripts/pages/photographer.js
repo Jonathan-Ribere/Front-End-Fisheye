f981b60e43cf806d38a8f4562a1cab50e01e336d
Jonathan Ribiere <j_ribiere@icloud.com>
2022-10-23 20:21:01 +0200

    refactoring

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -33,7 +33,77 @@ async function getMedia() {
 
   console.log(mediaId);
 }
+///////// FIN DE LA PARTIE JE DÉFINIE TOUTES LES FONCTION /////////////////
 
+///////// PARTIE AFFICHAGE (Display) /////////////////
+function displayHeader(photographer) {
+  /* Je séléctionne la partie 'main' */
+  const main = document.querySelector("main");
+  /* Je créer une div */
+  const bar = document.createElement("div");
+  /* Je lui ajoute une classe "bar" */
+  bar.classList.add("bar");
+  /* J'indique que "bar" et l'enfant de "main" */
+  main.appendChild(bar);
+
+  const barInfo = document.createElement("p");
+  barInfo.classList.add("barInfo");
+  bar.appendChild(barInfo);
+  barInfo.innerHTML = "bar";
+
+  // Je séléctionne la Class photograph-header
+  const photographerHeader = document.querySelector(".photograph-header");
+
+  // Je crée une section pour affichée les info du photographe
+  const divHeader = document.createElement("div");
+  divHeader.classList.add("divHeaderInfo");
+  photographerHeader.appendChild(divHeader);
+
+  // Je créer une div pour nom, pays, ville et tagline
+  const divInfo = document.createElement("div");
+  divInfo.classList.add("divInfo");
+  divHeader.appendChild(divInfo);
+
+  // Je créer un H1, city, country & tagline
+  const h1 = document.createElement("h1");
+  h1.classList.add("h1");
+  divInfo.appendChild(h1);
+  h1.innerHTML = photographer.name;
+
+  const city = document.createElement("p");
+  city.classList.add("city");
+  divInfo.appendChild(city);
+
+  const contry = document.createElement("p");
+  contry.classList.add("contry");
+  divInfo.appendChild(contry);
+
+  city.innerHTML = photographer.city + ", " + photographer.country;
+
+  const tagline = document.createElement("p");
+  tagline.classList.add("tagline");
+  divInfo.appendChild(tagline);
+  tagline.innerHTML = photographer.tagline;
+
+  //////////////////
+
+  const sectionImg = document.createElement("section");
+  photographerHeader.appendChild(sectionImg);
+
+  const divImg = document.createElement("div");
+  divInfo.classList.add("divImg");
+  sectionImg.appendChild(divImg);
+
+  const img = document.createElement("img");
+  const portrait = photographer.portrait;
+  console.log(portrait);
+  const picture1 = `../../assets/photographers/${portrait}`;
+  img.setAttribute("src", picture1);
+  img.classList.add("img");
+  divImg.appendChild(img);
+}
+
+///////// FIN PARTIE AFFICHAGE (Display) /////////////////
 // function final
 async function init() {
   const photographerId = getId();
@@ -42,5 +112,6 @@ async function init() {
   console.log(photographer);
   const media = await getMedia();
   console.log(media);
+  displayHeader(photographer);
 }
 init();
