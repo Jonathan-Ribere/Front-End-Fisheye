99999f4045d8aa086741f1ccfe5224ffc9ab875c
Jonathan Ribiere <j_ribiere@icloud.com>
2022-12-01 20:21:11 +0100

    function validation email

--- scripts/utils/formulaire.js
+++ scripts/utils/formulaire.js
@@ -8,25 +8,57 @@ nomPhotographe.innerHTML = data.name;*/
 // La récupération des éléments
 const form = document.querySelector("#form");
 
-// Ecouter les modfi
+// Ecouter les modfi sur l'input prénom
 form.prenom.addEventListener("change", function () {
   validPrenom(this);
 });
+// Ecouter les modfi sur l'input Nom
+form.nom.addEventListener("change", function () {
+  validPrenom(this);
+});
+// Ecouter les modfi sur l'input Email
+form.email.addEventListener("change", function () {
+  validEmail(this);
+});
 
+//VALIDATION DU PRÉNOM /////
 const validPrenom = function (inputPrenom) {
   // Création de la reg exp pour valid le prénom
   let prenomRegexp = new RegExp("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
 
-  // On teste l'expression reguliere
-  let testPrenom = prenomRegexp.test(inputPrenom.value);
+  // Récup de la balise small
   let small = inputPrenom.nextElementSibling;
 
-  if (testPrenom) {
-    small.innerHTML = "Prénom valide";
+  // On teste l'expression reguliere
+  if (prenomRegexp.test(inputPrenom.value)) {
+    small.innerHTML = "Champs valide";
     small.classList.remove("error");
     small.classList.add("success");
   } else {
-    small.innerHTML = "Prénom Invalide";
+    small.innerHTML = "Champs Invalide";
+    small.classList.remove("success");
+    small.classList.add("error");
+  }
+};
+
+//VALIDATION DE L'EMAIL /////
+const validEmail = function (inputEmail) {
+  // Création de la reg exp pour valid l'email
+  let emailRegexp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+    "g"
+  );
+
+  // Récup de la balise small
+  let small = inputEmail.nextElementSibling;
+
+  // On teste l'expression reguliere
+  if (emailRegexp.test(inputEmail.value)) {
+    small.innerHTML = "Email valide";
+    small.classList.remove("error");
+    small.classList.add("success");
+  } else {
+    small.innerHTML = "Email Invalide";
     small.classList.remove("success");
     small.classList.add("error");
   }
