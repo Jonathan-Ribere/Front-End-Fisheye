fc3eaff5d7dd55219e1e7cdfa32f3940d55ab0b0
Jonathan Ribiere <j_ribiere@icloud.com>
2023-02-01 23:31:04 +0100

    Je re part de zero

--- models/Media.js
+++
@@ -1,9 +0,0 @@
-class Media {
-  constructor(data) {
-    this._date = data.date;
-    this._id = data.id;
-    this._likes = data.likes;
-    this._photographerId = data.photographerId;
-    this._title = data.title;
-  }
-}
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,25 +1,46 @@
-//getPhotographers récupère la liste des photographes disponibles à partir d'un fichier JSON.
-async function getPhotographers() {
-  const response = await fetch("/data/photographers.json");
-  const data = await response.json();
-  return data;
-}
+    async function getPhotographers() {
+        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
+        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        let photographers = [
+            {
+                "name": "Ma data test",
+                "id": 1,
+                "city": "Paris",
+                "country": "France",
+                "tagline": "Ceci est ma data test",
+                "price": 400,
+                "portrait": "account.png"
+            },
+            {
+                "name": "Autre data test",
+                "id": 2,
+                "city": "Londres",
+                "country": "UK",
+                "tagline": "Ceci est ma data test 2",
+                "price": 500,
+                "portrait": "account.png"
+            },
+        ]
+        // et bien retourner le tableau photographers seulement une fois récupéré
+        return ({
+            photographers: [...photographers, ...photographers, ...photographers]})
+    }
 
-//displayData affiche la liste des photographes
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
 
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.display();
-    photographersSection.appendChild(userCardDOM);
-  });
-}
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
 
-async function init() {
-  // Récupération de la liste des photographes à partir du fichier JSON
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
-}
-
-init();
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
+    
+    init();
+    
