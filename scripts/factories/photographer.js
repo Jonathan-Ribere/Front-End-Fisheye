23cb943f600acc34cc10e3f43106bb3df43b6083
Jonathan Ribiere <j_ribiere@icloud.com>
2023-02-02 01:28:02 +0100

    Création de la partie Photo et Name du photographe de la page index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,12 +5,32 @@ function photographerFactory(data) {
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+
+    // Ajout de la photo et du Nom du photographe
+    const picAndNameContainerPhotographer = document.createElement("div");
+    picAndNameContainerPhotographer.classList.add(
+      "picAndNameContainerPhotographer"
+    );
+    article.appendChild(picAndNameContainerPhotographer);
+
+    const picPhotographer = document.createElement("img");
+    picPhotographer.setAttribute("src", picture);
+    picPhotographer.setAttribute("alt", "Photo de profile de : " + name);
+    picPhotographer.classList.add("picPotographer");
+    picAndNameContainerPhotographer.appendChild(picPhotographer);
+
+    const nameContainerPhotographer = document.createElement("div");
+    nameContainerPhotographer.classList.add("NameContainerPhotographer");
+    picPhotographer.appendChild(nameContainerPhotographer);
+    const namePhotographer = document.createElement("h2");
+    namePhotographer.classList.add("namePhotographer");
+    namePhotographer.innerHTML = name;
+    picAndNameContainerPhotographer.appendChild(namePhotographer);
+
+    // Ajout de la Ville du Tagline et du prix
+    const infoContainerPhotographer = document.createElement("div");
+    infoContainerPhotographer.classList.add("infoContainerPhotographer");
+
     return article;
   }
   return { name, picture, getUserCardDOM };
